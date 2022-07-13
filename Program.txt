893ffe8cda9bb294486bf41beef258d605bdff92
zafer eqbal <najish.eqbal@gmail.com>
2022-07-13 10:42:21 +0530

    new file are added

---
+++ Program.txt
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int a = scan.nextInt();
+int b = scan.nextInt();
+System.out.println("The value of a = " + a + " and b = " + b);
+}
+}
--- Test.c
+++ Test.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int sum(int,int);
+int factorial(int);
+
 int main()
 {
 int a = 10, b = 20;
 printf("%d", a + b);
 return 0;
+}
+
+
+
+int sum(int a,int b )
+{
+	return a + b;
+}
+
+
+
+int factorial (int number)
+{
+	return number * factorial(number - 1);
 }
